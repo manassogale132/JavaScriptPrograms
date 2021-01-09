0ddcd5dc4971942258b46c08bb530e0f52959f53
manassogale132 <manassogale@gmail.com>
2021-01-09 13:01:59 +0530

    Refactored.

--- empwage.js
+++ empwage.js
@@ -1,4 +1,4 @@
-function empWage(companyName,fullWage,halfWage,noWage,workingDays){       //functional constructor 'empWage'
+function empWage(companyName, fullWage, halfWage, noWage, workingDays) {       //functional constructor 'empWage'
 
     this.companyName = companyName;                   
     this.fullWage = fullWage;
@@ -6,36 +6,35 @@ function empWage(companyName,fullWage,halfWage,noWage,workingDays){       //func
     this.noWage = noWage;
     this.workingDays = workingDays;
 
-    this.empWageBuilder = function(){                                            //function / method (logic)
+    this.empWageBuilder = function() {                                         //function / method (logic)
 
-        console.log(' ');
-        console.log('------------------------------------------------------------------------------------------------');
+        
+        console.log('\n------------------------------------------------------------------------------------------------');
         console.log('-- Welcome to EmpWageBuilder program using above data --');
         console.log('------------------------------------------------------------------------------------------------');
-        console.log('Day No' + '            ' + 'Wage' + '                 ' + 'TotalWage');
-        console.log(' ');
-
+        console.log('Day No' +'\t\t\t'+ 'Wage' +'\t\t\t'+ 'TotalWage\n');
+        
         var sum = 0;
 
-        for(var i = 0; i<=workingDays; i++){
+        for(var i = 0; i <= workingDays; i++) {
 
            var attendance = Math.floor((Math.random() * 3) + 1);
           
-           switch(attendance){
+           switch(attendance) {
 
             case 1:
                 sum = sum + noWage;
-                console.log('Day ' + i + '               ' + noWage + '                    ' + sum);
+                console.log('Day ' + i +'\t\t\t'+ noWage +'\t\t\t'+ sum);
                 break;
             
             case 2:
                 sum = sum + halfWage;
-                console.log('Day ' + i + '               ' + halfWage + '                    ' + sum);
+                console.log('Day ' + i +'\t\t\t'+ halfWage +'\t\t\t'+ sum);
                 break;
             
             case 3:
                 sum = sum + fullWage;
-                console.log('Day ' + i + '               ' + fullWage + '                    ' + sum);
+                console.log('Day ' + i +'\t\t\t'+ fullWage +'\t\t\t'+ sum);
                 break;
             
             default:
@@ -44,16 +43,12 @@ function empWage(companyName,fullWage,halfWage,noWage,workingDays){       //func
 
           }
         }
-        console.log(' ');
-        console.log('Total Employee Wage is : ' + sum + '$');
-        console.log(' ');
-        console.log('------------------------------------------------------------------------------------------------');
-    }
-    
+        console.log('\nTotal Employee Wage is : ' + sum + '$\n'+
+                    '------------------------------------------------------------------------------------------------');
+    }  
 }
 
-var flipkart = new empWage("Flipkart",160,80,0,28);             //making an object of functional constructor and passing data as arguments
-console.log(' ');
+var flipkart = new empWage("Flipkart", 160, 80, 0, 28);         //making an object of functional constructor and passing data as arguments
 console.table(flipkart);                                        //just printing the data itself
  
 flipkart.empWageBuilder();                                      //calling the logic method or function within the functional constructor
